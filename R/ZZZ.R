a63bd3d6f910db0d113a8345626abc8c10aab379
jbbonifacio <45653134+jbbonifacio@users.noreply.github.com>
2023-06-22 10:36:44 +0800

    Delete R directory

--- R/ZZZ.R
+++
@@ -1,5 +0,0 @@
-.onAttach <- function(...) {
-
-	packageStartupMessage("\nWelcome to Plant Breeding Tools Design (PBToolsDesign)\n", "Copyright (c) 2022 International Rice Research Institute\n\n")
-}
-
